5391b383dc11616dadd440f870ca8cefd23d5dfe
Karel <145646267+Karel122@users.noreply.github.com>
2025-04-24 16:19:20 +0200

    Create grid.py

---
+++ examples/grid.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+
+
+
+myLabel1 = Label(root, text="Hello World").grid(row=0, column=0)
+myLabel2 = Label(root, text="Ime mi je Karel").grid(row=1, column=0)
+
+
+
+
+root.mainloop()
