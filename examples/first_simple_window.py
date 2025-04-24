d21bcaf2ba1d0461f84ec79a38629000cbf9d3c4
Karel <145646267+Karel122@users.noreply.github.com>
2025-04-24 15:53:52 +0200

    Create first_simple_window.py

---
+++ examples/first_simple_window.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+root = Tk()
+
+##always two steps 1. define 2. show it on the screen
+
+myLabel = Label(root, text="Hello World") #creating a label widget
+
+
+myLabel.pack() #shoving it to the screen
+
+
+## 3. create an event loop
+
+root.mainloop()
